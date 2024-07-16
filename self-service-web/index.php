f561fe2ec37a7ce971b1800701a07c925661503d
Brice Bauer <bauzer714@gmail.com>
2024-07-16 13:47:36 -0400

    Support archive of log file

--- self-service-web/index.php
+++ self-service-web/index.php
@@ -9,10 +9,36 @@ function doRebootIfNeeded() : bool {
    return false;
 }
 
+function isLogTypeArchive() : bool {
+   $type = strtolower(trim($_GET['logtype'] ?? ''));
+   return $type === 'archive';
+}
+
+function getLogContent() : string|bool {
+   $logfilecontents = file_get_contents('/mnt/log.txt');
+   if ($logfilecontents === false || !isLogTypeArchive()) {
+      return $logfilecontents;
+   }
+	$archivelogfilename = "log.txt";
+	$zip = new ZipArchive();
+	$zipfilename = 'log.zip';
+	$zipfilelocation = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $zipfilename;
+	if (file_exists($zipfilelocation)) {
+		unlink($zipfilelocation);
+	}
+	if ($zip->open($zipfilelocation, ZipArchive::CREATE) !== TRUE) {
+		return 'Failed to create zip archive';
+	}
+	$zip->addFromString($archivelogfilename, $logfilecontents);
+	$zip->close();
+	return base64_encode(file_get_contents($zipfilelocation));
+}
+
 $response = (object)array(
    'uptime' => shell_exec('who -b'),
    'reboot' => doRebootIfNeeded(),
-   'content' => file_get_contents('/mnt/log.txt'),
+   'content' => getLogContent(),
+   'isContentArchived' => isLogTypeArchive(),
 );
 header('Content-Type: application/json');
 echo json_encode($response);
